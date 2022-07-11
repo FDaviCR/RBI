22d4470586520d7717411938971be75596e4880e
Davi <davicamiloribeiro@gmail.com>
2022-07-10 22:57:09 -0300

    Controller e Model de User para controle de acesso

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define('users',{
+    login:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    active:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+})
+
+User.sync({force:false});
+
+module.exports = User;
